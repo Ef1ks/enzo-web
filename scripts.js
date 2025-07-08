18c97b8aacd017ef1b4ecfab2d3c498a5029c3c8
juleczkaishappy <julkamaja2003@wp.pl>
2025-07-08 12:20:16 +0200

    zrobione sekcje: o nas oraz produkty

--- scripts.js
+++ scripts.js
@@ -1,8 +1,43 @@
 window.addEventListener("scroll", () => {
     const nav = document.querySelector("nav");
+    const logo = document.querySelector(".logo");
+
     if (window.scrollY > 0) {
         nav.style.height = "45px";
+        logo.style.transform = "scale(0.5)";
+
+
     } else {
-        nav.style.height = "60px";
+        nav.style.height = "100px";
+        logo.style.transform = "scale(1)";
+
     }
 });
+
+/* slider */
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let current = 0;
+
+function showSlide(index)
+{
+	slides.forEach((slide, i)=> 
+	{
+		slide.classList.toggle('active', i == index)
+	});
+}
+
+prevButton.addEventListener('click',() =>
+{
+	current = (current === 0) ? slides.length -1 : current -1;
+	showSlide(current);
+});
+
+nextButton.addEventListener('click', () =>
+{
+	current = (current === slides.length -1) ? 0: current + 1;
+	showSlide(current);
+});
+
+showSlide(current);
